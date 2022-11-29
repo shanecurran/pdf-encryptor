747d363de9e35187759cb24182df2de3225a958a
Shane Curran <shane@evervault.com>
2022-11-29 15:54:51 +0000

    upload to transfer.sh

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const axios = require("axios");
+const FormData = require('form-data');
 
 exports.handler = async (data, context) => {
     const { data: pdf } = await axios.get(data.pdf);
     const encryptedPdf = await context.encrypt(pdf);
 
+    const form = new FormData();
+    form.append('file', Buffer.from(encryptedPdf), 'file.pdf');
+
+    const { data: uploaded } = await axios.put('https://transfer.sh/file.pdf', form, {
+        headers: {
+            ...form.getHeaders()
+        }
+    });
+
     return {
-        encryptedPdf,
+        encryptedPdf: uploaded,
         name: data.name
     };
 };
